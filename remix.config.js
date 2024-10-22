c9c61db0b85a5cbc44185b5dd1a9049049850914
shredder1970 <nelson.delpozo.dev@gmail.com>
2024-10-21 23:12:13 -0400

    update remix config

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
+module.exports = {
+  // Specifies the build target as Vercel
   serverBuildTarget: 'vercel',
+  
+  // Adjust the server file used in production
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+
+  // Add any additional configuration options your app requires
+  ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
+
+  cacheDirectory: "./node_modules/.cache/remix",
 };
